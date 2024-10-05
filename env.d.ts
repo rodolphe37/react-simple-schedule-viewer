663de561f13986446bb8c958aacebc0f0917ce54
rodolphe <rodolphe@MacBookPro-001.lan>
2024-10-05 23:39:13 +0200

    added some infos to the README file

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,5 @@
 // @types/react-simple-schedule-viewer@*
 
-// declare module 'react-simple-schedule-viewer';
-
-// declare module 'react-error-boundary'
-
-// declare module 'react-router-dom'
 
 declare module '*.css';
 declare module '*.module.css';
